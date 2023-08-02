ea010ca25cf84334438729368e759666a2af3666
Agwanyanjaba <bonkewycliffe@yahoo.com>
2023-08-02 09:18:33 +0300

    initial

---
+++ src/test/java/com/wybosoft/msbanking/MsBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wybosoft.msbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
